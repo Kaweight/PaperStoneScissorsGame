c0921b89c0da67ef50cc2d3fcf692d9a531fb1a7
Kamil Mazelanik <kamilmaz9206@gmail.com>
2020-08-19 11:02:45 +0200

    add script for player movement

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//losowanie ruchu komputera
 var computerMove, randomNumber;
 randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('wylosowana liczba to: ' + randomNumber);
@@ -11,3 +12,18 @@ if (randomNumber == '1') {
     computerMove = 'nieznany ruch';
 }
 printMessage('Mój ruch: ' + computerMove);
+
+//ustalenie ruchu gracza
+var playerInput, playerMove;
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '1') {
+    playerMove = 'kamień';
+} else if (playerInput == '2') {
+    playerMove = 'papier';
+} else if (playerInput == '3') {
+    playerMove = 'nożyczki';
+} else {
+    playerMove = 'nieznany ruch';
+}
+printMessage('Twój ruch: ' + playerMove);
